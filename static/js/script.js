2b37f87ebe58f32ba970ebd917bf58969b4cf148
Patrick O'Doherty <patricius.aod@gmail.com>
2024-09-04 16:44:45 +0000

    Add flash message jquery

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,36 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+// Flash Message Animation
+$(document).ready(function() {
+    const $messages = $('.flash-message');
+    let delay = 0;
+    
+    $messages.each(function(index, message) {
+        setTimeout(function() {
+            $(message).addClass('show');
+        }, delay);
+        delay += 2000; // Adjust delay as needed
+    });
+
+    // Remove messages after they have been shown
+    setTimeout(function() {
+        $messages.each(function(index, message) {
+            $(message).removeClass('show');
+            setTimeout(function() {
+                $(message).remove();
+            }, 500); // Remove after animation
+        });
+    }, delay + 2000); // Adjust total time before removing messages
+
+    // Handle close button clicks
+    $('.close-btn').click(function() {
+        const $message = $(this).closest('.flash-message');
+        $message.addClass('hide');
+        setTimeout(function() {
+            $message.remove();
+        }, 500); // Remove after animation
+    });
+
+   
+});
