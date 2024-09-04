afffdaf9bda28e50ac2f55bfebf1538744a2ecec
Patrick O'Doherty <patricius.aod@gmail.com>
2024-09-04 09:20:57 +0000

    Add default route signup form

--- app.py
+++ app.py
@@ -16,3 +16,29 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("MONGO_SECRET_KEY")
 
 mongo = PyMongo(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    
+    form_id = request.form.get('form_id')
+
+    if form_id == 'sign-up':
+        if request.method == 'POST':
+
+            existing_user = mongo.db.users.find_one({'username': request.form.get('username','').lower()})
+
+            #check if username exists in the database   
+            if existing_user:
+                flash('Username already exists. Please try again.', 'error')
+                return redirect(url_for('index'))
+            
+            #insert new user into the database
+            new_user = {
+                    'username': request.form.get('username'),
+                    'password': generate_password_hash(request.form.get('password')),
+                    'role': 'user'
+                    }
+            mongo.db.users.insert_one(new_user)
+
+            flash('Registration successful! You can now log in.', 'success')
+            return render_template('index.html')
