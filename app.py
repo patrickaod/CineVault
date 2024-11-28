791c17039a28c5ecf799dc39a98f97ebba67444b
Patrick O'Doherty <patricius.aod@gmail.com>
2024-11-28 12:55:55 +0000

    Improve error handling, code readability for ObjectId validation and form logic

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
+from bson.errors import InvalidId
 from datetime import datetime
 import os
 import re
@@ -21,14 +22,12 @@ mongo = PyMongo(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        form_id = request.form.get('form_id')
+        if form_id == 'sign-up':
 
-    form_id = request.form.get('form_id')
-
-    if form_id == 'sign-up':
-        if request.method == 'POST':
-
-            existing_user = mongo.db.users.find_one
-            ({'username': request.form.get('username', '').lower()})
+            existing_user = mongo.db.users.find_one(
+                {'username': request.form.get('username', '').lower()})
 
             # check if username exists in the database
             if existing_user:
@@ -37,30 +36,31 @@ def index():
 
             # insert new user into the database
             new_user = {
-                    'username': request.form.get('username'),
-                    'password': generate_password_hash
-                    (request.form.get('password')),
-                    'role': 'user'
-                    }
+                'username': request.form.get('username'),
+                'password': generate_password_hash
+                (request.form.get('password')),
+                'role': 'user'
+            }
             mongo.db.users.insert_one(new_user)
 
             flash('Registration successful! You can now log in.', 'success')
             return render_template('index.html')
 
-    elif form_id == 'login':
-        if request.method == 'POST':
-            existing_user = mongo.db.users.find_one({'username': request.form.get('username', '').lower()})
+        elif form_id == 'login':
+            existing_user = mongo.db.users.find_one(
+                {'username': request.form.get('username', '').lower()})
 
             if existing_user:
                 role = existing_user.get('role')
 
                 if role == "user":
                     # ensure hashed password is correct
-                    if check_password_hash(existing_user['password'], request.form.get('password')):
+                    if check_password_hash(existing_user['password'], request.form.get('password')):  # noqa
                         # put the new user into 'session' cookie
                         session["user"] = request.form.get('username').lower()
-                        flash('Login successful! You are now log in.', 'success')
-                        return redirect(url_for("account", username=session["user"]))
+                        flash('Login successful! You are now log in.', 'success')  # noqa
+                        return redirect(url_for(
+                            "account", username=session["user"]))
                     else:
                         # invalid password match
                         flash("Incorrect Username and/or Password")
@@ -76,13 +76,17 @@ def index():
 def account(username):
     if request.method == 'POST':
         form_id = request.form.get('form_id')
-
+        movie_id = request.form.get('movie_id')
         if form_id == 'movie_user_add':
-            # Get the title from the form and convert it to lowercase
-            movie_title = request.form.get('title', '').lower()
-
             # Find the movie in the database
-            movie_check = mongo.db.movies.find_one({'created_by': session["user"], 'title': {'$regex': re.escape(movie_title), '$options': 'i'}})
+            try:
+                # Validate and convert movie_id to ObjectId
+                object_id = ObjectId(movie_id)
+                movie_check = mongo.db.movies.find_one({'_id': object_id})
+            except InvalidId:
+                # Handle invalid ObjectId
+                movie_check = False
+            # Initialise watchlist
             watchlist = "yes" if request.form.get("watchlist") else "no"
 
             if movie_check:
@@ -94,7 +98,8 @@ def account(username):
                     "watchlist": watchlist,
                     'updated_on': datetime.now().strftime('%d/%m/%Y')
                 }
-                mongo.db.movies.update_one({'_id': movie_check['_id']}, {'$set': updated_movie})
+                mongo.db.movies.update_one(
+                    {'_id': movie_check['_id']}, {'$set': updated_movie})
                 flash("Movie Successfully Updated", "success")
             else:
                 # Add new movie
@@ -116,23 +121,25 @@ def account(username):
 
         if movie_id:
             # Delete the movie by its ID
-            delete_result = mongo.db.movies.delete_one
-            ({'_id': ObjectId(movie_id)})
+            delete_result = mongo.db.movies.delete_one(
+                {'_id': ObjectId(movie_id)})
 
             if delete_result.deleted_count > 0:
                 flash("Movie Successfully Deleted", "success")
             else:
                 flash("Failed to delete movie", "error")
 
-    movies = mongo.db.movies.find({"created_by": session["user"]}).sort("created_on", 1)
+    movies = mongo.db.movies.find({
+        "created_by": session["user"]}).sort("created_on", 1)
 
     try:
         # Attempt to grab the session user's username from the database
-        username = mongo.db.users.find_one
-        ({"username": session["user"]})["username"]
+        username = mongo.db.users.find_one(
+            {"username": session["user"]})["username"]
 
         # If the session is valid, render the account page
-        return render_template("account.html", username=username, movies=movies)
+        return render_template(
+            "account.html", username=username, movies=movies)
 
     except KeyError:
         # If the session does not have the "user" key, redirect to the index
@@ -159,9 +166,11 @@ def search_movies(username):
         }).sort("created_on", 1)
     else:
         # If search query is empty, retrieve all movies
-        movies = mongo.db.movies.find({"created_by": session["user"]}).sort("created_on", 1)
+        movies = mongo.db.movies.find(
+            {"created_by": session["user"]}).sort("created_on", 1)
 
-    return render_template("account.html", username=session["user"], movies=movies)
+    return render_template(
+        "account.html", username=username, movies=movies)
 
 
 @app.route("/logout")
